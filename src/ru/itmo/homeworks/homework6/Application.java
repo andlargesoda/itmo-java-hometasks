da6f7d9dd1b16db800b2a28f202428a975585c87
Igor Belykh <igor.a.belykh@gmail.com>
2023-03-29 01:20:55 +0300

    добавлен класс Group

--- src/ru/itmo/homeworks/homework6/Application.java
+++ src/ru/itmo/homeworks/homework6/Application.java
@@ -5,6 +5,7 @@ import ru.itmo.homeworks.homework6.hikes.Group;
 
 public class Application {
     public static void main(String[] args) {
+        Group group01 = new Group();
+        group01.addClimber01({"fdsfds","fdsafas","fsdafsdf","fdasfa"}, true);
     }
-
 }
--- src/ru/itmo/homeworks/homework6/hikes/Group.java
+++ src/ru/itmo/homeworks/homework6/hikes/Group.java
@@ -2,5 +2,15 @@ package ru.itmo.homeworks.homework6.hikes;
 
 public class Group {
     private Mountain mountain;
+    private Climber[] climbers = new Climber[5];
+    private boolean isOpen;
 
+    public void addClimber01(Climber[] climbers, boolean isOpen) { // при вызове данного метода в Application в () необходимо указать массив альпинистов, открытость набора в группу)
+        if (isOpen) {
+            System.out.println("Набор в группу еще есть");
+            for (int i = 0; i < climbers.length; i++) {
+                this.climbers = climbers; // на значение, указанное при вызове данного метода будет i - элемент массива
+            }
+        }
+    }
 }
