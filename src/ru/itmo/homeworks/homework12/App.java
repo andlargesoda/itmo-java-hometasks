91f22ca7d80b8e419cd73986a6e3ff622ba5b4de
Igor Belykh <igor.a.belykh@gmail.com>
2023-04-14 00:48:57 +0300

    Добавлена задача 1 (generic типы) 12

--- src/ru/itmo/homeworks/homework12/App.java
+++ src/ru/itmo/homeworks/homework12/App.java
@@ -11,7 +11,7 @@ public class App {
         System.out.println(pairContainer01.getValue());
 
         // Создать экземпляр PairContainer, где key - строка, value - тип User, у которого id - любые числа
-        PairContainer<String, User<Number>> pairContainer02= new PairContainer<>();
+        PairContainer<String, User<Number>> pairContainer02 = new PairContainer<>();
         User<Number> numberUser = new User<>();
         numberUser.setId(2);
         pairContainer02.setKey("2");
@@ -31,5 +31,56 @@ public class App {
         pairContainer03.setValue(pairContainer04);
         System.out.println(pairContainer03.getKey());
         System.out.println(pairContainer03.getValue());
+
+        /* Задача 2
+        interface Eatable extends Serializable{}
+        interface Runnable {}
+
+        class Animal implements Runnable{}
+
+        class Cat extends Animal implements Eatable{}
+        class Kitten extends Cat{}
+
+        class Dog extends Animal implements Serializable{}
+
+
+        class Task {
+
+            // в классе можно вызывать generic тип и его родителей
+            public static  <T extends Cat & Eatable> void void01(T object){
+                // ВОПРОС: методы каких типов можно вызвать у object ???
+                // Object
+                // Animal
+                // Cat
+                // Runnable
+                // Serializable
+                // Eatable
+            }
+
+            public static  <T extends Animal & Serializable> void void02(T object){
+                // ВОПРОС: методы каких типов можно вызвать у object ???
+                // Object
+                // Animal
+                // Runnable
+                // Serializable
+            }
+
+            public static  <T extends Serializable & Runnable> void void03(T object){
+                // ВОПРОС: методы каких типов можно вызвать у object ???
+                // Object
+                // Runnable
+                // Serializable
+            }
+         */
+        /*
+            public static void main(String[] args) {
+
+            // в точке входа в программу можно вызывать generic тип и его детей
+         */
+        //        Task.<Cat, Kitten>void01(cat, kitten);
+        //
+        //      Task.<Cat, Kitten,Dog>void02(cat, kitten, dog);
+        //  Task.<Dog>void03(dog);
+
     }
 }
